ac8f3317e4137074b40ac3ae60ac11b058c15ac6
KhairulAzry <mkhairulazry@gmail.com>
2022-11-15 11:58:00 +0800

    Add Main Section

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
 
-  <div class="flex flex-col w-full h-full min-h-screen">
+  <!-- <div class="flex flex-col w-full h-full min-h-screen">
     <div class="flex-1 w-full h-full flex flex-wrap items-center justify-center bg-black">
         <div class="space-y-2 text-white text-center">
             <div class="text-h3 md:text-h1 font-black uppercase">Coming Soon</div>
             <div>This page is under construction. <br/> Thank you for visiting.</div>
         </div>
     </div>
-  </div>
- 
-  <!-- <div class="min-h-screen h-full w-full bg-white">
-    <div class="w-full md:w-96 mx-auto">
-      <div class="bg-local bg-no-repeat bg-center h-screen w-full bg-cover" :style="mainBg"></div>
-    </div>
   </div> -->
+ 
+  <div class="min-h-screen h-full w-full bg-dark font-garamond">
+    <div class="w-full md:w-container mx-auto">
+
+      <!-- Main Section -->
+      <div class="bg-local bg-no-repeat bg-center h-screen w-full bg-cover" :style="mainBg">
+        <div class="flex flex-wrap w-full h-full items-center justify-center">
+          <div class="flex flex-col items-center text-center text-white space-y-1.5">
+            <div class="uppercase">Walimatul Urus</div>
+            <div class="text-h1 text-gold font-cinzel space-y-0">
+              <div>Alya</div>
+              <div class="font-alex">&</div>
+              <div>Azry</div>
+            </div>
+            <div class="space-y-0.25">
+              <div class="uppercase">Jumaat</div>
+              <div class="flex items-center justify-center space-x-1">
+                <div class="border-y-2 border-white">FEB</div>
+                <div class="text-h1 pb-1.75">3</div>
+                <div class="border-y-2 border-white">2023</div>
+              </div>
+            </div>
+            <div class="uppercase">Noor Arfa Craft Complex<br>Kuala Terengganu</div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Next section -->
+    </div>
+  </div>
 
 </template>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,62 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    fontSize: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      base: "1rem",
-      lg: "1.25rem",
-      xl: "1.5rem",
-      "h1": "3.75rem",
-      "h2": "2rem",
-      "h3": "1.75rem",
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      dark: '#242424',
+      gold: '#EEBB55',
     },
-    extend: {},
+    fontSize: {
+      xs: ["0.75rem", "0.75rem"],
+      sm: ["0.875rem", "0.875rem"],
+      base: ["1rem", "1rem"],
+      lg: ["1.25rem", "1.25rem"],
+      xl: ["1.5rem", "1.5rem"],
+      "h1": ["3.75rem", "3.75rem"],
+      "h2": ["2rem", "2rem"],
+      "h3": ["1.75rem", "1.75rem"],
+    },
+    width: {
+      0: 0,
+      "container": "450px",
+      auto: "auto",
+      full: "100%",
+      screen: "100vw",
+    },
+    height: {
+      0: 0,
+      auto: "auto",
+      full: "100%",
+      screen: "100vh",
+    },
+    spacing: {
+      0: "0",
+      0.25: "0.25rem",
+      0.5: "0.5rem",
+      0.75: "0.75rem",
+      1: "1rem",
+      1.5: "1.5rem",
+      1.75: "1.75rem",
+      2: "2rem",
+      3: "3rem",
+    },
+    extend: {
+      fontFamily: {
+        'cinzel': ['"Cinzel"', 'serif'],
+        'alex': ['"Alex Brush"', 'cursive'],
+        'garamond': ['"Cormorant Garamond"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
